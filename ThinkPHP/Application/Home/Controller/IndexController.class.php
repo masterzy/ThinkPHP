c1d4d24bb5eef08112eef9b82dd0b1adb388c6fe
ZhengTong <zt19930302@yeah.net>
2016-12-10 00:14:54 +0800

    debug true

--- ThinkPHP/Application/Home/Controller/IndexController.class.php
+++ ThinkPHP/Application/Home/Controller/IndexController.class.php
@@ -3,6 +3,9 @@ namespace Home\Controller;
 use Think\Controller;
 class IndexController extends Controller {
     public function index(){
+        for($i = 0;$i <= 10; $i++){
+            echo $i . '<br />';
+        }
         $this->display();
     }
 }
